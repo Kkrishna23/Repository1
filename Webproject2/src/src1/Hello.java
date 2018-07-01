1a0ea1014e0aa0a23e143fc1c4d5746093d61aa7
vamshi <krishh.v471@gmail.com>
2018-07-01 16:41:11 +0530

    pushed by vamshi6

--- Webproject2/src/src1/Hello.java
+++ Webproject2/src/src1/Hello.java
@@ -5,12 +5,16 @@ public class Hello {
 	public static void main(String args[])
 	{
 		
-		System.out.println("HellojnZNXnjn");
-		
+		try{  
+		    int a[]=new int[5];  
+		    a[5]=30/0;  
+		   }  
+		   catch(ArithmeticException e){System.out.println("difference");}  
+		   catch(ArrayIndexOutOfBoundsException e){System.out.println("task 2 completed");}  
+		   catch(Exception e){System.out.println("common task completed");}  
+		  
+		   System.out.println("rest of the code...");  
 
-		// helloo2233
-
-		
-	}
+}
 
 }
