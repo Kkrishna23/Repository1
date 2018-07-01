044f477823406a2f30f13e828f3a1b9ac2c1849f
vamshi <krishh.v471@gmail.com>
2018-07-01 16:53:37 +0530

    pushed by vamshi

--- Webproject2/src/src1/Hello.java
+++ Webproject2/src/src1/Hello.java
@@ -9,17 +9,13 @@ public class Hello {
 		    int a[]=new int[5];  
 		    a[5]=30/0;  
 		   }  
-		   catch(ArithmeticException e){System.out.println("difference");}  
+		   catch(ArithmeticException e){System.out.println("task1 is completed  by krishna");}  
 		   catch(ArrayIndexOutOfBoundsException e){System.out.println("task 2 completed");}  
 		   catch(Exception e){System.out.println("common task completed");}  
 		  
-		   System.out.println("rest of the code...");  
+		   System.out.println("rest of the code by krishna");  
 		   
-		 System.out.println("difference");
-		  System.out.println("task 2 completed");
-		 System.out.println("common task completed"); 
-		  
-		   System.out.println("rest of the code...");  
+		 
 
 }
 
