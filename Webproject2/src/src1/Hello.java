fd8174481c765e66b18bcf3ce3d05e77c7b4f812
krishna <devulapalli.krishnachaitanya@gmail.com>
2018-07-01 16:41:50 +0530

    pushed by krishna 6

--- Webproject2/src/src1/Hello.java
+++ Webproject2/src/src1/Hello.java
@@ -5,9 +5,17 @@ public class Hello {
 	public static void main(String args[])
 	{
 		
-		System.out.println("HellojnZNXnjn");
+		try{  
+		    int a[]=new int[5];  
+		    a[5]=30/0;  
+		   }  
+		   catch(ArithmeticException e){System.out.println("task1 is completed");}  
+		   catch(ArrayIndexOutOfBoundsException e){System.out.println("task 2 completed");}  
+		   catch(Exception e){System.out.println("common task completed");}  
+		  
+		   System.out.println("rest of the code...");  
 		
-		// hello krishna 1111
+		
 	}
 
 }
