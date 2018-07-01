beff75f57f46f9004ab7d460677d73ebb8307abe
krishna <devulapalli.krishnachaitanya@gmail.com>
2018-07-01 11:20:23 +0530

    Pushing Webproject2

---
+++ Webproject2/src/src1/Hello.java
@@ -0,0 +1,11 @@
+package src1;
+
+public class Hello {
+	
+	public static void main(String args[])
+	{
+		
+		System.out.println("HellojnZNXnjn");
+	}
+
+}
---
+++ Webproject2/src/src1/Hello2.java
@@ -0,0 +1,16 @@
+package src1;
+
+public class Hello2 {
+	
+	public static void main(String args[])
+	{
+		
+	
+
+	
+	System.out.println("hello2");
+	
+	// hello 222
+	
+	}
+}
