ae3a209c2295fc8b322fb028108f9ed1cfccb5d5
krishna <devulapalli.krishnachaitanya@gmail.com>
2018-09-23 17:09:11 +0530

    Second commit by first developer

--- Webproject2/src/src1/Hello.java
+++ Webproject2/src/src1/Hello.java
@@ -4,7 +4,9 @@ public class Hello {
 	
 	public static void main(String args[])
 	{
-		   System.out.println("No 1");  
+		   System.out.println("No 1"); 
+		   
+		   System.out.println("No 2"); 
 
 }
 	
